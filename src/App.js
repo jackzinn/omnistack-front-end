6e99d89ce4882dc1561f7dea066c008928eef2ae
Kelvin <kelvin.musselli@iv2.com.br>
2019-06-03 23:13:13 -0300

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+// import Main from './pages/Main';
+import Routes from './routes';
+
+// function App() {
+//   return <Main />;
+// }
+
+const App = () => <Routes />;
+
+export default App;
